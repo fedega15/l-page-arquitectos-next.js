d54fa39f5b126e95686042bab1c101a2e9e5c4da
Federico Galassi - Front End Dev <94568896+fedega15@users.noreply.github.com>
2023-09-16 01:01:52 -0300

    entregado

--- src/app/Contacto/page.js
+++ src/app/Contacto/page.js
@@ -47,8 +47,16 @@ const page = () => {
                 height={482}
               />
             </div>
+            
           </div>
         ))}
+          <div>
+            <a href="https://www.instagram.com/fedegalassi_/" className="redes" ><i className="fa-brands fa-instagram"></i></a>
+            <a href="https://wa.link/d08xt8" className="redes" ><i class="fa-brands fa-whatsapp"></i></a>
+            <a href="https://www.instagram.com/fedegalassi_/" className="redes" ><i className="fa-brands fa-linkedin"></i></a>
+            <a href="https://www.instagram.com/fedegalassi_/" className="redes" ><i className="fa-brands fa-facebook-messenger"></i></a>
+            <a href="https://maps.app.goo.gl/3oR6RPvDhTtfmDm79" className="redes" ><i className="fa-solid fa-location-dot"></i> </a>
+            </div>
       </div>
     </section>
   );
