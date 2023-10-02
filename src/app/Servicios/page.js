c79ad4b45940b1550e016579551a73f94b0f7ff3
Federico Galassi - Front End Dev <94568896+fedega15@users.noreply.github.com>
2023-10-01 21:40:56 -0300

    asd

--- src/app/Servicios/page.js
+++ src/app/Servicios/page.js
@@ -1,83 +1,77 @@
 import React from "react";
-import "../../../styles/servicios.css"
+import "../../../styles/servicios.css";
 import Image from "next/image";
 
 export const galeria = [
   {
     id: 1,
-    proyect: "Exibicion de arte",
-    name: "SONICO",
-    ubi: "galeria de arte sidney",
-    img: "/serv1.jpg",
-    activites: 10,
-    places: 12,
-    days: "1week",
-    year: 2023,
-    link: "/Exibicion",
-    servicio:"Arquitectura",
-    descripcion:"Diseñamos en base a lineamientos estéticos, pensamos en cada etapa del proyecto atendiendo a todos los detalles para que la obra sea perfectamente ejecutable.Buscamos constantemente la innovación en el uso e implementación de materiales."
+    name: "Interiores / Diseño de edificios",
+    links: [
+      { name: "Hotel Nhow Berlín", link: "/ASD4" },
+      { name: "Fun Factory Múnich", link: "/ASD5" },
+      { name: "kado", link: "/ASD6" },
+    ],
+    descripcion:
+      "Como empresa de diseño industrial que ha crecido en el diseño de interiores y edificios durante los últimos 15 años, hemos evolucionado hasta convertirnos en una empresa de diseño de servicio completo, que ayuda a nuestros clientes a alcanzar sus objetivos en una amplia gama de tipologías de proyectos y ubicaciones globales. Nuestro estudio cuenta con un equipo internacional de talentosos diseñadores y contamos con el respaldo de una variada cartera de consultores globales para cualquier tipo de proyecto. Diseñamos a todas las escalas, desde la residencia privada hasta el imponente edificio de uso mixto. Estamos equipados para apoyar a nuestros clientes con un servicio integrado durante todo el ciclo de vida de un proyecto.",
   },
   {
     id: 2,
-    proyect: "Moda",
-    name: "APAGAR",
-    ubi: "galeria de arte auckland",
-    img: "/serv2.jpg",
-    activites: 10,
-    places: 12,
-    days: "1week",
-    year: 2021,
-    link: "/Moda",
-    servicio:"Construccion",
-    descripcion:"Gestionamos de manera eficiente el proceso constructivo completo, buscando el máximo rendimiento de los recursos, disminuyendo los costos y minimizando los tiempos."
+    name: "Diseño industrial",
+    links: [
+      { name: "Hotel Nhow Berlín", link: "/ASD4" },
+      { name: "Fun Factory Múnich", link: "/ASD5" },
+      { name: "kado", link: "/ASD6" },
+    ],
+    descripcion:
+      "En cada proyecto encontramos la solución que fusiona nuestra visión con la experiencia de nuestros clientes para dar vida a objetos hermosos, funcionales, fabricables, progresivos y económicamente viables. Trabajamos con grandes corporaciones y empresas emergentes para crear y desarrollar conceptos innovadores en prácticamente todas las categorías de bienes de consumo. Desde el inicio del concepto hasta el desarrollo del diseño, los detalles, la creación de prototipos y la orientación en la fabricación, nuestro equipo de diseñadores industriales tiene una amplia experiencia en la creación de diseños que mejoran las experiencias de la vida cotidiana.",
   },
   {
     id: 3,
-    proyect: "Productos",
-    name: "TOTEM",
-    ubi: "galeria de arte dobrazil",
-    img: "/serv3.jpg",
-    activites: 10,
-    places: 12,
-    days: "1week",
-    year: 1999,
-    link: "/Productos",
-    servicio:"Gerenciamiento",
-    descripcion:"Aseguramos la ejecución de la obra comprometiéndonos con la calidad planificada y cumpliendo con los plazos estipulados."
+    name: "Gráfico / Marca",
+    links: [
+      { name: "Hotel Nhow Berlín", link: "/ASD4" },
+      { name: "Fun Factory Múnich", link: "/ASD5" },
+      { name: "kado", link: "/ASD6" },
+    ],
+    descripcion:
+      "Nos especializamos en patronaje, estampado, branding y dirección creativa. Nuestros clientes trabajan directamente con el equipo creativo en una variedad de proyectos, lo que permite no incumplir plazos ni realizar trabajos en el extranjero. Producimos diseños que ayudan a crear o revitalizar marcas que se destacan a través de una variedad de medios impresos y otros. Dependiendo de la naturaleza del proyecto, la gráfica se entrelaza en el diseño del producto y del interior. Tenemos la capacidad de diseñar un proyecto bajo un mismo techo, lo que permite un proceso más fluido y un diseño holístico..",
   },
-  
 ];
 
-const page = () => {
+
+const Page = () => {
   return (
     <section id="galery">
       <div className="heading">
-        <h2>QUE HACEMOS</h2>
-      </div> 
-      <div className="horizontal-line"></div>
+        <h1>SERVICIOS</h1>
+      </div>
       <div className="grid">
-        {galeria.map((galeria, index) => (
-          <div className="cont" key={galeria.id}> 
-          <a key={galeria.id} href={`/photo/${index}`} className="active">
-            <div className={`card card-${index}`} key={galeria.id}>
-              <Image
-                src={galeria.img}
-                alt={galeria.img}
-                width={600}
-                height={600}
-              />
-             
+        {galeria.map((proyecto, index) => (
+          <div className="cont" key={proyecto.id}>
+            <div className="item">
+              <div className="horizontal-line-2"></div>
+              <h2>{proyecto.name}</h2>
             </div>
-            <h1>{galeria.servicio}</h1>
-            <p>{galeria.descripcion}</p>
-          </a>
+            <div className="item">
+              <p>{proyecto.descripcion}</p>
+            </div>
+            <div className="item">
+              <h1>Links Destacados:</h1>
+              <ul>
+                {proyecto.links.map((enlace, i) => (
+                  <li key={i}>
+                    <a href={enlace.link} className="link-destacado">
+                      {enlace.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         ))}
       </div>
     </section>
-    
-    
   );
 };
 
-export default page;
+export default Page;
