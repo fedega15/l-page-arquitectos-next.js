73a786578967df071fdba08b80804523ad34d82a
Federico Galassi - Front End Dev <94568896+fedega15@users.noreply.github.com>
2023-10-30 22:02:39 -0300

    asd

---
+++ components/Prensa.js
@@ -0,0 +1,37 @@
+// Card.js
+import Image from "next/image";
+import React from "react";
+
+const Card = ({ img, number, mes, año, title,tipo, enlace }) => {
+  return (
+    <div>
+    <Image
+      src={img}
+      alt={img}
+      width={200}
+      height={200}
+    />
+    <div className="article-header">
+      <div className="date">
+        <span className="date-number">{number}</span>
+        <span className="date-number2">{mes} </span>
+        <span className="date-number3">{año} </span>
+      </div>
+      <div className="article-content">
+        <h2>{title}</h2>
+        <p>{tipo}</p>
+
+        <a
+          target="_blank"
+          href={enlace}
+          className="press-link"
+        >
+          Leer más
+        </a>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/app/Prensa/page.js
+++ src/app/Prensa/page.js
@@ -1,56 +1,61 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import { motion, useAnimation } from "framer-motion";
-import Image from "next/image";
+import Prensa from "../../../components/Prensa";
 import "../../../styles/acerca.css";
 
 export const galeria = [
-  { 
-    img:"/serv1.jpg",
-    number:11,
+  {
+    img: "/serv1.jpg",
+    number: 11,
     mes: "Junio",
     año: 2016,
     title: " Construcción Sustentable U.T.N.",
-    tipo:"CHARLA",
-    enlace: "https://noticias.perfil.com/noticias/general/2019-09-18-santiago-semino-arquitectos-nuevas-formas-de-habitar.phtml"
+    tipo: "CHARLA",
+    enlace:
+      "https://noticias.perfil.com/noticias/general/2019-09-18-santiago-semino-arquitectos-nuevas-formas-de-habitar.phtml",
   },
-  { 
-    img:"/photo3.jpg",
-    number:28,
+  {
+    img: "/photo3.jpg",
+    number: 28,
     mes: "Agosto",
     año: 2019,
     title: " Revista Noticias",
-    tipo:"NOTA",
-    enlace: "https://noticias.perfil.com/noticias/general/2019-09-18-santiago-semino-arquitectos-nuevas-formas-de-habitar.phtml"
+    tipo: "NOTA",
+    enlace:
+      "https://noticias.perfil.com/noticias/general/2019-09-18-santiago-semino-arquitectos-nuevas-formas-de-habitar.phtml",
   },
-  { 
-    img:"/serv2.jpg",
-    number:26,
+  {
+    img: "/serv2.jpg",
+    number: 26,
     mes: "Mayo",
     año: 2020,
     title: "D.Y Patil University (Pune-India)",
-    tipo:"LECTURE",
-    enlace: "https://drive.google.com/file/d/1VOTVjd2oSF_yYT8HyGO5b0-sTzZ10KRj/view"
+    tipo: "LECTURE",
+    enlace:
+      "https://drive.google.com/file/d/1VOTVjd2oSF_yYT8HyGO5b0-sTzZ10KRj/view",
   },
-  { 
-    img:"/serv3.jpg",
-    number:16,
+  {
+    img: "/serv3.jpg",
+    number: 16,
     mes: "Marzo",
     año: 2023,
     title: "Terrenos y Quintas ",
-    tipo:"NOTA",
-    enlace: "https://notas.terrenosyquintas.com/construccion-en-seco-la-tendencia-que-reduce-costos/"
+    tipo: "NOTA",
+    enlace:
+      "https://notas.terrenosyquintas.com/construccion-en-seco-la-tendencia-que-reduce-costos/",
   },
-  { 
-    img:"/photo2.jpg",
-    number:30,
+  {
+    img: "/photo2.jpg",
+    number: 30,
     mes: "Junio",
     año: 2023,
     title: "Mundo Construcción",
-    tipo:"ENTREVISTA",
-    enlace: "https://open.spotify.com/episode/3JjW62ePXmv7W1ATY3r4nk?si=Ev-5LthNRx-3NDiOGf3TaQ"
+    tipo: "ENTREVISTA",
+    enlace:
+      "https://open.spotify.com/episode/3JjW62ePXmv7W1ATY3r4nk?si=Ev-5LthNRx-3NDiOGf3TaQ",
   },
-]
+];
 const Page = () => {
   const [scrollY, setScrollY] = useState(0);
   const [isLoaded, setIsLoaded] = useState(false);
@@ -86,14 +91,13 @@ const Page = () => {
   useEffect(() => {
     // Define the scroll threshold where you want to apply the animation
     const scrollThreshold = 200; // Adjust this threshold as needed
-  
+
     if (scrollY >= scrollThreshold) {
       textAnimationControls.start({ x: -100, opacity: 0 });
     } else {
       textAnimationControls.start({ x: 0, opacity: 1 });
     }
   }, [scrollY, textAnimationControls]);
-
 
   return (
     <section className="grid" id="galery">
@@ -108,36 +112,19 @@ const Page = () => {
       </motion.div>
       <br />
       <div className="press-grid">
-      {galeria.map((galeria, i) => (
-      <div key={i} className="press-article">
-      <Image
-        src={galeria.img}
-        alt={galeria.img}
-        width={200}
-        height={200}
-      />
-      <div className="article-header">
-        <div className="date">
-          <span className="date-number">{galeria.number}</span>
-          <span className="date-number2">{galeria.mes} </span>
-          <span className="date-number3">{galeria.año} </span>
-        </div>
-        <div className="article-content">
-          <h2>{galeria.title}</h2>
-          <p>{galeria.tipo}</p>
-
-          <a
-            target="_blank"
-            href={galeria.enlace}
-            className="press-link"
-          >
-            Leer más
-          </a>
-        </div>
-      </div>
-    </div>
-          ))}
-        
+        {galeria.map((galeria, i) => (
+          <div key={i} className="press-article">
+            <Prensa
+              img={galeria.img}
+              number={galeria.number}
+              mes={galeria.mes}
+              año={galeria.año}
+              title={galeria.title}
+              tipo={galeria.tipo}
+              enlace={galeria.enlace}
+            />
+          </div>
+        ))}
       </div>
     </section>
   );
