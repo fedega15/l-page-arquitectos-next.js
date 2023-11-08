b474806e906990b17b54840bfb7a352f77a73d3c
Federico Galassi - Front End Dev <94568896+fedega15@users.noreply.github.com>
2023-11-08 18:56:40 -0300

    ASD

--- src/app/Arquitectura-photo/[slug]/page.js
+++ src/app/Arquitectura-photo/[slug]/page.js
@@ -1,36 +1,45 @@
 import Image from 'next/image'
-import { galeria } from '@/app/Arquitectura/page'
+/* import { galeria } from '@/app/Arquitectura/page' */
 import React from "react";
 import "../../../../styles/photopage.css"
+import { fetchArquitecturaData } from "../../../lib/dataFetcher";
 
 const PhotoPage = ({params: { slug }}) => {
-  console.log(galeria[slug].photo1)
-  console.log(galeria[slug].img)
+  const [arquitecturaData, setArquitecturaData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchArquitecturaData();
+      setArquitecturaData(data);
+    }
+    fetchData();
+  }, []);
+
       return (
         <section id="testimonios">
           
           <div className="slider">
-            <h2>{galeria[slug].name}</h2>
-            <h1>{galeria[slug].ubi}</h1>
+            <h2>{arquitecturaData[slug].name}</h2>
+            <h1>{arquitecturaData[slug].ubi}</h1>
             <p className='crossed-lines'>
               	{galeria[slug].descripcion}
             </p>
           </div>
           <div className="img-container">
             <a href="https://maps.app.goo.gl/3oR6RPvDhTtfmDm79">
-              <Image   src={galeria[slug].photo1} alt="sub" width={458} height={452} border={2} />
+              <Image   src={arquitecturaData[slug].photo1} alt="sub" width={458} height={452} border={2} />
             </a>
             <a href="https://maps.app.goo.gl/3oR6RPvDhTtfmDm79">
-              <Image   src={galeria[slug].photo2} alt="sub" width={458} height={452} border={2} />
+              <Image   src={arquitecturaData[slug].photo2} alt="sub" width={458} height={452} border={2} />
             </a>
           </div>
           <div className="img-container">
           <div className="line-bottom-right"></div>
             <a href="https://maps.app.goo.gl/3oR6RPvDhTtfmDm79">
-              <Image   src={galeria[slug].photo3}alt="sub" width={458} height={452} border={2} />
+              <Image   src={arquitecturaData[slug].photo3}alt="sub" width={458} height={452} border={2} />
             </a>
             <a href="https://maps.app.goo.gl/3oR6RPvDhTtfmDm79">
-              <Image  src={galeria[slug].photo4} alt={galeria[slug]}width={458} height={452} border={2} />
+              <Image  src={arquitecturaData[slug].photo4} alt={arquitecturaData[slug]}width={458} height={452} border={2} />
             </a>
           </div>
         </section>
