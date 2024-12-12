085f05ba4f145668e80b5fea0e421214a8210a85
Federico Galassi - Front End Dev <94568896+fedega15@users.noreply.github.com>
2024-12-12 14:33:23 +0000

    Added three buttons to the welcome banner section and adjusted the button styles. Ensured the text color in the experience-text section is white.

--- components/Galery.js
+++ components/Galery.js
@@ -1,67 +1,74 @@
-  "use client";
-  import "../styles/galery.css";
-  import Image from "next/image";
-  import { galery } from "./data";
+"use client";
+import "../styles/galery.css";
+import Image from "next/image";
+import { galery } from "./data";
 
-  const Galery = () => {
-    return (
-      <div className="wrapper">
-        {/* Banner con logo y descripción */}
-        <section id="banner">
-          <div className="banner-content">
-            <div className="text-overlay right">
-              <h1>Bienvenidos a SSA</h1>
-              <p>
-                Un estudio de arquitectura innovador especializado en
-                soluciones sostenibles y diseños personalizados. Descubre a continuación nuestra galería de proyectos destacados.
-              </p>
-              <a href="#galery" className="arrow-down">↓</a> {/* Flecha clickeable */}
+const Galery = () => {
+  return (
+    <div className="wrapper">
+      {/* Banner con logo y descripción */}
+      <section id="banner">
+        <div className="banner-content">
+          <div className="text-overlay right">
+            <h1>Bienvenidos a SSA</h1>
+            <p>
+              Un estudio de arquitectura innovador especializado en
+              soluciones sostenibles y diseños personalizados. Descubre a continuación nuestra galería de proyectos destacados.
+            </p>
+            <div className="buttons-container">
+            <a href="#galery"  className="button">Galeria de arquitectura</a>
             </div>
-
-            <div className="image-container">
-              <Image
-                src="/A6.jpg" // Coloca aquí la ruta correcta de tu imagen
-                alt="Welcome Image"
-                layout="fill"
-                objectFit="cover"
-                className="background-image"
-              />
-            </div>
+            <a href="#galery" className="arrow-down">↓</a> {/* Flecha clickeable */}
           </div>
-        </section>
-        <section id="galery">
-          <div className="grid">
-            {galery.map((galeria, index) => (
-              <div className={`card card-${index}`} key={galeria.id}>
-                <a href={galeria.link} className={`active card card-${index}`}>
-                  <Image
-                    src={galeria.img}
-                    alt={galeria.img}
-                    width={220}
-                    height={100}
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                    className="imagen-galeria"
-                  />
-                  <div className="overlay">
-                    <span className="ver-mas">Ver más</span>
-                  </div>
-                  <div className="box">
-                    <div className="group">
-                      <div className="text">
-                        <span>{galeria.places}</span>
-                      </div>
+
+          <div className="image-container">
+            <Image
+              src="/A6.jpg" // Coloca aquí la ruta correcta de tu imagen
+              alt="Welcome Image"
+              layout="fill"
+              objectFit="cover"
+              className="background-image"
+            />
+          </div>
+        </div>
+        <div className="experience-text">
+          <h5>25 años de experiencia</h5>
+        </div>
+      </section>
+
+      <section id="galery">
+        <div className="grid">
+          {galery.map((galeria, index) => (
+            <div className={`card card-${index}`} key={galeria.id}>
+              <a href={galeria.link} className={`active card card-${index}`}>
+                <Image
+                  src={galeria.img}
+                  alt={galeria.img}
+                  width={220}
+                  height={100}
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  className="imagen-galeria"
+                />
+                <div className="overlay">
+                  <span className="ver-mas">Ver más</span>
+                </div>
+                <div className="box">
+                  <div className="group">
+                    <div className="text">
+                      <span>{galeria.places}</span>
                     </div>
                   </div>
-                  <div className="title">
-                    <strong>{galeria.name}</strong>
-                  </div>
-                </a>
-              </div>
-            ))}
-          </div>
-        </section>
-      </div>
-    );
-  };
+                </div>
+                <div className="title">
+                  <strong>{galeria.name}</strong>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
 
-  export default Galery;
+export default Galery;
