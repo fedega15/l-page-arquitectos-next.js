0e0ae2e265b82839de2fdddc4b7845c8007104af
Federico Galassi - Front End Dev <94568896+fedega15@users.noreply.github.com>
2024-12-12 13:32:37 +0000

    Adjusted CSS for better responsiveness on larger screens
    - Increased `max-width` for larger screens to reduce excessive margins.
    - Reduced `padding` for the body on larger screens.
    - Modified grid column sizes for better layout on high-resolution displays.
    - Ensured consistent appearance across different screen sizes.

--- components/Galery.js
+++ components/Galery.js
@@ -1,68 +1,67 @@
-"use client";
-import "../styles/galery.css";
-import Image from "next/image";
-import { galery } from "./data";
+  "use client";
+  import "../styles/galery.css";
+  import Image from "next/image";
+  import { galery } from "./data";
 
-const Galery = () => {
-  return (
-    <div className="wrapper">
-      {/* Banner con logo y descripción */}
-      <section id="banner">
-        <div className="banner-content">
-          <div className="text-overlay right">
-            <h1>Bienvenidos a SSA</h1>
-            <p>
-              Un estudio de arquitectura innovador especializado en
-              soluciones sostenibles y diseños personalizados. Descubre a continuación nuestra galería de proyectos destacados.
-            </p>
-            <a href="#galery" className="arrow-down">↓</a> {/* Flecha clickeable */}
+  const Galery = () => {
+    return (
+      <div className="wrapper">
+        {/* Banner con logo y descripción */}
+        <section id="banner">
+          <div className="banner-content">
+            <div className="text-overlay right">
+              <h1>Bienvenidos a SSA</h1>
+              <p>
+                Un estudio de arquitectura innovador especializado en
+                soluciones sostenibles y diseños personalizados. Descubre a continuación nuestra galería de proyectos destacados.
+              </p>
+              <a href="#galery" className="arrow-down">↓</a> {/* Flecha clickeable */}
+            </div>
+
+            <div className="image-container">
+              <Image
+                src="/A6.jpg" // Coloca aquí la ruta correcta de tu imagen
+                alt="Welcome Image"
+                layout="fill"
+                objectFit="cover"
+                className="background-image"
+              />
+            </div>
           </div>
-
-          <div className="image-container">
-            <Image
-              src="/A6.jpg" // Coloca aquí la ruta correcta de tu imagen
-              alt="Welcome Image"
-              layout="fill"
-              objectFit="cover"
-              className="background-image"
-            />
-          </div>
-        </div>
-      </section>
-
-      <section id="galery">
-        <div className="grid">
-          {galery.map((galeria, index) => (
-            <div className={`card card-${index}`} key={galeria.id}>
-              <a href={galeria.link} className={`active card card-${index}`}>
-                <Image
-                  src={galeria.img}
-                  alt={galeria.img}
-                  width={220}
-                  height={100}
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                  className="imagen-galeria"
-                />
-                <div className="overlay">
-                  <span className="ver-mas">Ver más</span>
-                </div>
-                <div className="box">
-                  <div className="group">
-                    <div className="text">
-                      <span>{galeria.places}</span>
+        </section>
+        <section id="galery">
+          <div className="grid">
+            {galery.map((galeria, index) => (
+              <div className={`card card-${index}`} key={galeria.id}>
+                <a href={galeria.link} className={`active card card-${index}`}>
+                  <Image
+                    src={galeria.img}
+                    alt={galeria.img}
+                    width={220}
+                    height={100}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    className="imagen-galeria"
+                  />
+                  <div className="overlay">
+                    <span className="ver-mas">Ver más</span>
+                  </div>
+                  <div className="box">
+                    <div className="group">
+                      <div className="text">
+                        <span>{galeria.places}</span>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="title">
-                  <strong>{galeria.name}</strong>
-                </div>
-              </a>
-            </div>
-          ))}
-        </div>
-      </section>
-    </div>
-  );
-};
+                  <div className="title">
+                    <strong>{galeria.name}</strong>
+                  </div>
+                </a>
+              </div>
+            ))}
+          </div>
+        </section>
+      </div>
+    );
+  };
 
-export default Galery;
+  export default Galery;
